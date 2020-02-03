1829bb8ebdf41f730aabb0f8c1541eac5fe041dd
linocrusher <egrelayson@up.edu.ph>
2020-02-03 12:51:27 +0800

    Added menu page.

--- main/views.py
+++ main/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'main/index.html')
+
+def menu(request):
+	return render(request, 'main/menu.html', {'title': 'Menu'})
