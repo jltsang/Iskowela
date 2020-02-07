60f048264817acee6ca74eec4bd2db4887f0abc4
linocrusher <egrelayson@up.edu.ph>
2020-02-07 19:56:03 +0800

    Fixed secret key

--- SrSeA/settings.py
+++ SrSeA/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '1)%0!vgz6(pn*bmwz=r12a%)s!o3qj*b_!ss1bw!drhl$0q+q$'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '1)%0!vgz6(pn*bmwz=r12a%)s!o3qj*b_!ss1bw!drhl$0q+q$'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
