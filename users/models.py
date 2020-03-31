b87999619afcfc3c1bd6b8e959ca1baf6bba0df3
linocrusher <egrelayson@up.edu.ph>
2020-03-31 22:58:46 +0800

    Added about page + other misc

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	school_name = models.CharField(max_length=40)
+	logo = models.ImageField(default='default.jpg', upload_to='logo_pics')
+	contact_details = models.TextField()
+	mapbox_key = models.CharField(max_length=100)
+	new_game_dataset_link = models.CharField(max_length=80)
+	event_markers_dataset_link = models.CharField(max_length=80)
+	place_markers_dataset_link = models.CharField(max_length=80)
+	live_chat_link = models.CharField(max_length=50)
+	chatbot_tree_link = models.CharField(max_length=50)
+
+	def __str__(self):
+		return f'{self.user.username} Profile'
