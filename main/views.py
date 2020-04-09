2d9e47d49793fd9e58a82b1d687b42426ec9e749
linocrusher <linocrusher@gmail.com>
2020-04-09 20:08:54 +0800

    Profile now editable. Auto-resize and link logo images.

--- main/views.py
+++ main/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+from users.models import Profile
+
+active_profile = Profile.objects.get(school_name="Roosevelt College Marikina")
 
 def index(request):
-	return render(request, 'main/index.html')
+	context = {
+		'active_profile': active_profile,
+	}
+	return render(request, 'main/index.html', context)
 
 def menu(request):
-	return render(request, 'main/menu.html', {'title': 'Menu'})
+	context = {
+		'title': 'Menu',
+		'active_profile': active_profile,
+	}
+	return render(request, 'main/menu.html', context)
 
 def imap_menu(request):
-	return render(request, 'main/imap_menu.html', {'title': 'Interactive Map Menu'})
+	context = {
+		'title': 'Interactive Map Menu',
+		'active_profile': active_profile,
+	}
+	return render(request, 'main/imap_menu.html', context)
--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
 	email = forms.EmailField()
@@ -8,4 +9,15 @@ class UserRegisterForm(UserCreationForm):
 	class Meta:
 		model = User  
 		fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+	email = forms.EmailField()
+
+	class Meta:
+		model = User  
+		fields = ['username', 'email']
 		
+class ProfileUpdateForm(forms.ModelForm):
+	class Meta:
+		model = Profile
+		fields = ['school_name', 'logo', 'contact_details', 'mapbox_key', 'event_markers_dataset_link', 'newgame_markers_dataset_link', 'health_markers_dataset_link', 'food_markers_dataset_link', 'finance_markers_dataset_link', 'store_markers_dataset_link', 'etc_markers_dataset_link', 'newgame_textfield', 'live_chat_link', 'chatbot_tree_link']
