05677656c7540b6745cffd08b853c1f47d3b39f6
linocrusher <egrelayson@up.edu.ph>
2020-04-13 21:07:06 +0800

    Restored code past heroku migration.

--- SrSeA/urls.py
+++ SrSeA/urls.py
@@ -26,26 +26,26 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
 	path('', include('main.urls')),
-    # path('chatbot/', chatbot_views.index, name='chatbot-index'),
-    # path('admin/', admin.site.urls),
+    path('chatbot/', chatbot_views.index, name='chatbot-index'),
+    path('admin/', admin.site.urls),
 
-    # path('imap/<int:stype>', interactive_map_views.SuggestionListView.as_view(), name='imap-index'),
+    path('imap/<int:stype>', interactive_map_views.SuggestionListView.as_view(), name='imap-index'),
     # path('imap/<int:pk>', interactive_map_views.SuggestionDetailView.as_view(), name='suggestion-detail'), #Deletable / Not really
-    # path('imap/new', interactive_map_views.SuggestionCreateView.as_view(), name='suggestion-create'),
-    # path('imap/<int:pk>/update/', interactive_map_views.SuggestionUpdateView.as_view(), name='suggestion-update'),
-    # path('imap/<int:pk>/delete/', interactive_map_views.SuggestionDeleteView.as_view(), name='suggestion-delete'),
-    # path('imap/newgame', interactive_map_views.newgame, name='imap-newgame'),
+    path('imap/new', interactive_map_views.SuggestionCreateView.as_view(), name='suggestion-create'),
+    path('imap/<int:pk>/update/', interactive_map_views.SuggestionUpdateView.as_view(), name='suggestion-update'),
+    path('imap/<int:pk>/delete/', interactive_map_views.SuggestionDeleteView.as_view(), name='suggestion-delete'),
+    path('imap/newgame', interactive_map_views.newgame, name='imap-newgame'),
 
-    # path('register/', user_views.register, name='register'), #Link to (create new admin)
-    # path('profile/', user_views.profile, name='profile'),
-    # path('pupdate/', login_required(user_views.pupdate), name='pupdate'), #Admin only
+    path('register/', user_views.register, name='register'), #Link to (create new admin)
+    path('profile/', user_views.profile, name='profile'),
+    path('pupdate/', login_required(user_views.pupdate), name='pupdate'), #Admin only
 
-    # path('ssr/', login_required(ssr_views.SSRListView.as_view()), name='ssr-index'), #Admin only
-    # path('ssr/new', ssr_views.SSRCreateView.as_view(), name='ssr-create'),
-    # path('ssr/<int:pk>/delete/', login_required(ssr_views.SSRDeleteView.as_view()), name='ssr-delete'), #Admin only
+    path('ssr/', login_required(ssr_views.SSRListView.as_view()), name='ssr-index'), #Admin only
+    path('ssr/new', ssr_views.SSRCreateView.as_view(), name='ssr-create'),
+    path('ssr/<int:pk>/delete/', login_required(ssr_views.SSRDeleteView.as_view()), name='ssr-delete'), #Admin only
 
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    # path('logout/', login_required(auth_views.LogoutView.as_view(template_name='users/logout.html')), name='logout'), #Signed in only
+    path('logout/', login_required(auth_views.LogoutView.as_view(template_name='users/logout.html')), name='logout'), #Signed in only
 ]
 
 if settings.DEBUG:
