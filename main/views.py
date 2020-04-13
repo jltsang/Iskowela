966dfa2db533bdd0cb4b89696456855d15fd9fcc
linocrusher <egrelayson@up.edu.ph>
2020-04-14 00:31:20 +0800

    Restored active_profile in main. Must be logged in to access /register path.

--- main/views.py
+++ main/views.py
@@ -1,24 +1,24 @@
 from django.shortcuts import render
 from users.models import Profile
 
-# active_profile = Profile.objects.get(school_name="Roosevelt College Marikina")
+active_profile = Profile.objects.get(school_name="Roosevelt College Marikina")
 
 def index(request):
 	context = {
-		# 'active_profile': active_profile,
+		'active_profile': active_profile,
 	}
 	return render(request, 'main/index.html', context)
 
 def menu(request):
 	context = {
 		'title': 'Menu',
-		# 'active_profile': active_profile,
+		'active_profile': active_profile,
 	}
 	return render(request, 'main/menu.html', context)
 
 def imap_menu(request):
 	context = {
 		'title': 'Interactive Map Menu',
-		# 'active_profile': active_profile,
+		'active_profile': active_profile,
 	}
 	return render(request, 'main/imap_menu.html', context)
