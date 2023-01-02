c573ac86d60c1b81641f0926b312ef72e69aafda
Jeremy Tsang <jlt.jkltsang@gmail.com>
2023-01-02 19:02:48 +0800

    minor bug fix and initial table creation for toggling

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Toggles
+
+admin.site.register(Toggles)
+
 # Register your models here.
--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Toggles(models.Model):
+    school_name = models.CharField(max_length=40)
+    info_toggle = models.BooleanField(default=False)
+    processguides_toggle = models.BooleanField(default=False)
+    scholarships_toggle = models.BooleanField(default=False)
+    courses_toggle = models.BooleanField(default=False)
+    markers_toggle = models.BooleanField(default=False)
+    chatbot_toggle = models.BooleanField(default=False)
+    web_analytics_toggle = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.school_name
